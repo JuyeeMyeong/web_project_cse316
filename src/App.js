38e5fc682d9685d256879af349112ab4432d19a0
JuyeeMyeong <juyee.ming@gmail.com>
2023-05-07 17:46:40 +0900

    #2 use Route create Instructions, Login, PreviousCourses, SelectCourses components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, {useState} from 'react';
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Select } from '@mui/material';
 //components
 import Home from './components/Home';
+import Instructions from './components/Instructions';
+import Login from './components/Login';
+import PreviousCourses from './components/PreviousCourses';
+import SelectCourses from './components/SelectCourses';
+
 
 function App() {
   return (
@@ -10,6 +16,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/Instructions" element={<Instructions />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/PreviousCourses" element={<PreviousCourses />} />
+          <Route path="/SelectCourses" element={<SelectCourses />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/PreviousCourses.js
@@ -0,0 +1,17 @@
+import '../App.css';
+import React, { useState, useEffect }  from 'react';
+import { Link } from 'react-router-dom';
+
+function PreviousCourses() {
+
+    return (
+        <div>
+
+
+
+
+        </div>
+    );
+};
+
+export default PreviousCourses;
