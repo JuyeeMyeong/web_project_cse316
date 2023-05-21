d7543fb2c148ad376853b256c442841cf7231d9e
JuyeeMyeong <juyee.ming@gmail.com>
2023-05-21 19:51:59 +0900

    #2 NavLink design changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,23 +7,25 @@ function Home() {
     return (
         <div>
             <h2 className='text-center'>CourseMan!</h2>
-            <header className="header d-flex justify-content-end">
-                <h1>
-                    <NavLink to="/" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>Home</NavLink>
-                </h1>
-                <h1>
-                    <NavLink to="/Instructions" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>Instructions</NavLink>
-                </h1>
-                <h1>
-                    <NavLink to="/Login" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>Login</NavLink>
-                </h1>
-                <h1>
-                    <NavLink to="/PreviousCourses" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>PreviousCourses</NavLink>
-                </h1>
-                <h1>
-                    <NavLink to="/SelectCourses" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>SelectCourses</NavLink>
-                </h1>
-            </header>
+            <div className='header-container'>
+                <header className="header d-flex justify-content-end align-items-center">
+                    <h1>
+                        <NavLink to="/" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>Home</NavLink>
+                    </h1>
+                    <h1>
+                        <NavLink to="/Instructions" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>Instructions</NavLink>
+                    </h1>
+                    <h1>
+                        <NavLink to="/Login" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>Login</NavLink>
+                    </h1>
+                    <h1>
+                        <NavLink to="/PreviousCourses" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>PreviousCourses</NavLink>
+                    </h1>
+                    <h1>
+                        <NavLink to="/SelectCourses" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>SelectCourses</NavLink>
+                    </h1>
+                </header>
+            </div>
 
 
 
--- src/components/SelectCourses.js
+++ src/components/SelectCourses.js
@@ -10,23 +10,25 @@ function SelectCourses() {
     return (
         <div>
             <h2 className='text-center'>CourseMan!</h2>
-            <header className="header d-flex justify-content-end">
-                <h1>
-                    <NavLink to="/" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>Home</NavLink>
-                </h1>
-                <h1>
-                    <NavLink to="/Instructions" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>Instructions</NavLink>
-                </h1>
-                <h1>
-                    <NavLink to="/Login" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>Login</NavLink>
-                </h1>
-                <h1>
-                    <NavLink to="/PreviousCourses" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>PreviousCourses</NavLink>
-                </h1>
-                <h1>
-                    <NavLink to="/SelectCourses" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>SelectCourses</NavLink>
-                </h1>
-            </header>
+            <div className='header-container'>
+                <header className="header d-flex justify-content-end">
+                    <h1>
+                        <NavLink to="/" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>Home</NavLink>
+                    </h1>
+                    <h1>
+                        <NavLink to="/Instructions" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>Instructions</NavLink>
+                    </h1>
+                    <h1>
+                        <NavLink to="/Login" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>Login</NavLink>
+                    </h1>
+                    <h1>
+                        <NavLink to="/PreviousCourses" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>PreviousCourses</NavLink>
+                    </h1>
+                    <h1>
+                        <NavLink to="/SelectCourses" className={({isActive}) => (isActive? "activeNav" : "defaultNav")} style={{ textDecoration: "none" }}>SelectCourses</NavLink>
+                    </h1>
+                </header>
+            </div>
 
             <div className='searchForm'>
                 <div className='formTitle'>Search Form</div>
