6ad9770f86514bd9dc170c68dab7694f5a61a363
juyeon6069 <97582404+juyeon6069@users.noreply.github.com>
2023-05-22 22:24:00 +0900

    #1 implementing login function in backend

--- db/server.js
+++ db/server.js
@@ -21,3 +21,44 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 app.listen(PORT, () => {
   console.log(`Check out the app at http://localhost:${PORT}`);
 });
+
+// Login
+app.post("/login", function (req, res) {
+  // get first and last name from body
+  const password = req.body.password;
+  con.query(
+    "SELECT * FROM User WHERE email = ?", //instead of email=?, use first and last name
+    [email], // change this also
+
+    function (err, data, field) {
+      if (err) throw err;
+      if (data.length === 0) {
+        res.status(401).json({
+          status: "Failed",
+          message: "No user found with the provided email.",
+        });
+        return;
+      }
+
+      const id = data[0].id;
+      const studentId = data[0].student_id;
+      const hashCheck = hashutil(email, password);
+
+      if (hashCheck !== data[0].password) {
+        res
+          .status(402)
+          .json({ status: "Failed", message: "Invalid password." });
+        return;
+      }
+
+      res.status(201).json({
+        status: "Success",
+        message: "Successfully logged in.",
+        firstName: first_name,
+        lastName: last_name,
+        id,
+        studentId,
+      });
+    }
+  );
+});
