cf273410cf98a1bcafc4772ea7773354814a205d
juyeon6069 <97582404+juyeon6069@users.noreply.github.com>
2023-05-22 23:50:27 +0900

    #3 set up databases - User & Course list table

---
+++ db/data.sql
@@ -0,0 +1,81 @@
+-- User Table --
+
+INSERT INTO User (student_id, first_name, last_name, password) 
+        VALUES (113659563, "Harry", "Chung", "");
+INSERT INTO User (student_id, first_name, last_name, password) VALUES 
+    (114631693, "Matt", "Lee", "");
+INSERT INTO User (student_id, first_name, last_name, password) VALUES 
+    (113539682, "SUE", "Park", "");
+INSERT INTO User (student_id, first_name, last_name, password) VALUES 
+    (123456789, 'John', 'Doe', "");
+INSERT INTO User (student_id, first_name, last_name, password) VALUES 
+    (234567891, 'Jane', 'Doe', "");
+INSERT INTO User (student_id, first_name, last_name, password) VALUES 
+    (345678912, 'Bob', 'Smith', "");
+INSERT INTO User (student_id, first_name, last_name, password) VALUES 
+    (456789123, 'Alice', 'Johnson', "");
+INSERT INTO User (student_id, first_name, last_name, password) VALUES 
+    (567891234, 'Charlie', 'Brown', "");
+INSERT INTO User (student_id, first_name, last_name, password) VALUES 
+    (678912345, 'Lucy', 'Van Pelt', "");
+INSERT INTO User (student_id, first_name, last_name, password) VALUES 
+    (789123456, 'Linus', 'Van Pelt', "");
+INSERT INTO User (student_id, first_name, last_name, password) VALUES 
+    (891234567, 'Sally', 'Brown', "");
+INSERT INTO User (student_id, first_name, last_name, password) VALUES 
+    (912345678, 'Marcie', 'Carlin', "");
+INSERT INTO User (student_id, first_name, last_name, password) VALUES 
+    (123456780, 'Peppermint Patty', 'Reichardt', "");
+
+
+-- Course Table --
+INSERT INTO courses (course_id, course_name)
+       VALUES ("CSE101", "Computer Science Principles");
+
+INSERT INTO courses (course_id, course_name)
+       VALUES ("CSE114", "Intro to Object-Oriented Programming");
+
+INSERT INTO courses (course_id, course_name)
+       VALUES ("CSE214", "Data Structures");
+
+INSERT INTO courses (course_id, course_name)
+       VALUES ("CSE215", "Foundation of Comp Science");
+
+INSERT INTO courses (course_id, course_name)
+       VALUES ("CSE216", "Programming Abstractions");
+
+INSERT INTO courses (course_id, course_name)
+       VALUES ("CSE220", "System Fundamentals I");
+
+INSERT INTO courses (course_id, course_name)
+       VALUES ("CSE300", "Technical Communications");
+
+INSERT INTO courses (course_id, course_name)
+       VALUES ("CSE303", "Intro to the Theory of Computation");
+
+INSERT INTO courses (course_id, course_name)
+       VALUES ("CSE304", "Compiler Design");
+
+INSERT INTO courses (course_id, course_name)
+       VALUES ("CSE310", "Computer Networks");
+
+INSERT INTO courses (course_id, course_name)
+       VALUES ("CSE316", "Fundamentals of Software Development");
+
+INSERT INTO courses (course_id, course_name)
+       VALUES ("CSE320", "System Fundamentals II");
+
+INSERT INTO courses (course_id, course_name)
+       VALUES ("CSE352", "Altificial Intelligence");
+       
+INSERT INTO courses (course_id, course_name)
+       VALUES ("CSE354", "Natural Language Processing");
+
+INSERT INTO courses (course_id, course_name)
+       VALUES ("CSE373", "Analysis of Algorithms");
+
+INSERT INTO courses (course_id, course_name)
+       VALUES ("CSE416", "Software Engineering");
+
+
+-- Prerequsite Table --
