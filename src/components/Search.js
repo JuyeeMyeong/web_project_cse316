0ecfa79de75b14044f958365f4d040bb6f590992
JuyeeMyeong <juyee.ming@gmail.com>
2023-05-22 23:54:05 +0900

    #2 design changes
    - Navbar sliding & fixed position
    - select courses button for test

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,17 +4,24 @@ import React, { useState, useEffect, useMemo }  from 'react';
 
 //list of courses can be taken 
 
-function Search () {
+function Search ({prevCourses}) {
 
     const [searchString, setSearchString] = useState("");
-    const [name, setName] = useState("");
+    const [name, setName] = useState("Tony");
 
     return(
         <div className='courseContainer'>
             <div className='listBox'>
-                <h4>{} here are the courses you may select.</h4>
-                <div>{}</div>
-                <button>Register</button>
+                <h4 id='searchHdr'>{name} here are the courses you may select.</h4>
+                <div className='d-flex flex-column'>
+                    {prevCourses.map((course, index) => (
+                        <div key={index}>
+                            <input type='checkbox' id={`course-${index}`} />
+                            <label htmlFor={`course-${index}`} style={{fontStyle: 'italic', fontWeight: '600'}}>{course.name}</label>
+                        </div>
+                    ))}
+                </div>
+                <button className='resgisterBtn'>Register</button>
             </div>
         </div>
     );
--- src/components/SelectCourses.js
+++ src/components/SelectCourses.js
@@ -5,30 +5,54 @@ import Navbar from './Navbar';
 
 function SelectCourses() {
 
-    return (
-    <div>
-        <Navbar />
-        <div className="selectCoursePage d-flex flex-column align-items-center">
+    const[prevCourses, setPrevCourses] = useState([
+        { name: 'CSE101' },
+        { name: 'CSE114' },
+        { name: 'CSE214' },
+        { name: 'CSE215' },
+        { name: 'CSE216' },
+        { name: 'CSE220' },
+        { name: 'CSE300' },
+        { name: 'CSE303' },
+        { name: 'CSE304' },
+        { name: 'CSE305' },
+        { name: 'CSE306' },
+        { name: 'CSE310' },
+        { name: 'CSE316' },
+        { name: 'CSE320' },
+        { name: 'CSE331' },
+        { name: 'CSE416' },
+    ]);
 
-            <div className="form-container d-flex flex-column">
-                <div className='searchForm'>
-                    <div className='formTitle'>Search Form</div>
-                    <div className='d-flex justify-content-between searchRow'>
-                        <label id="Name" htmlFor='' className="userName">Name:</label>
-                        <input className="searchInput" type="text" name="user" id="" placeholder='Name here...'></input>
+    const [showCourses, setShowCourses] = useState(false);
+
+    const handleClick = () => {
+        setShowCourses(!showCourses);
+    }
+
+    return (
+        <div>
+            <Navbar />
+            <div className="selectCoursePage d-flex flex-column align-items-center">
+                <div className="form-container d-flex flex-column">
+                    <div className='searchForm'>
+                        <div className='formTitle'>Search Form</div>
+                        <div className='d-flex justify-content-between searchRow'>
+                            <label id="Name" htmlFor='' className="userName">Name:</label>
+                            <input className="searchInput" type="text" name="user" id="" placeholder='Name here...'></input>
+                        </div>
+                        <div className='d-flex justify-content-between searchRow'>
+                            <label id="SearchFor" htmlFor='' className="searchBox">Search for:</label>
+                            <input className="searchInput" type="text" name="searchString" id="" placeholder='Name here...'></input>
+                        </div>
+                        <div>
+                            <button className='showCoursebtn' onClick={handleClick}>{!showCourses? 'Show Courses' : 'Hide Courses'}</button>
+                        </div>
                     </div>
-                    <div className='d-flex justify-content-between searchRow'>
-                        <label id="SearchFor" htmlFor='' className="searchBox">Search for:</label>
-                        <input className="searchInput" type="text" name="searchString" id="" placeholder='Name here...'></input>
-                    </div>
-                    <div>
-                        <button className='showCoursebtn'>Show Courses</button>
-                    </div>
+                    {showCourses && <Search prevCourses={prevCourses}/>}
                 </div>
-                <Search/>
             </div>
         </div>
-    </div>
     );
 };
 
