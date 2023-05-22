41939851a02fc494c60b471c9243cc5c2472b38f
juyeon6069 <97582404+juyeon6069@users.noreply.github.com>
2023-05-22 22:41:57 +0900

    #2 Remove mui

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
-import React, {useState} from 'react';
-import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import { Select } from '@mui/material';
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 //components
-import Home from './components/Home';
-import Instructions from './components/Instructions';
-import Login from './components/Login';
-import PreviousCourses from './components/PreviousCourses';
-import SelectCourses from './components/SelectCourses';
-
+import Home from "./components/Home";
+import Instructions from "./components/Instructions";
+import Login from "./components/Login";
+import PreviousCourses from "./components/PreviousCourses";
+import SelectCourses from "./components/SelectCourses";
 
 function App() {
-
-  const[isLoggedIn, setIsLoggedIn] = useState(false);
-  const[stuId, setStuId] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [stuId, setStuId] = useState("");
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home />} />
           <Route path="/Instructions" element={<Instructions />} />
-          <Route path="/Login" element={<Login stuId={stuId} setStuId={setStuId} setIsLoggedIn={setIsLoggedIn}/>}/>
+          <Route
+            path="/Login"
+            element={
+              <Login
+                stuId={stuId}
+                setStuId={setStuId}
+                setIsLoggedIn={setIsLoggedIn}
+              />
+            }
+          />
           <Route path="/PreviousCourses" element={<PreviousCourses />} />
-          <Route path="/SelectCourses" element={<SelectCourses />}/>
+          <Route path="/SelectCourses" element={<SelectCourses />} />
         </Routes>
       </BrowserRouter>
     </div>
