1a4af1581a64af48310d26c093c9d269070513ee
juyeon6069 <97582404+juyeon6069@users.noreply.github.com>
2023-05-23 23:08:20 +0900

    #3 update query of user & prereq table

--- db/data.sql
+++ db/data.sql
@@ -3,19 +3,19 @@
 INSERT INTO User (student_id, first_name, last_name, password, courses) 
         VALUES (113659563, "Harry", "Chung", "e71b258f98d6b92aef96ebb947b7843a199973603961c66b1f91d92b4c335e33",  '["CSE101", "CSE114"]');
 INSERT INTO User (student_id, first_name, last_name, password, courses) VALUES 
-    (114631693, "Matt", "Lee", "a24136aef3bad4fd8ddb24eundefined195a036283480eb5983e2350ede94e41863ee", "");
+    (114631693, "Matt", "Lee", "a24136aef3bad4fd8ddb24eundefined195a036283480eb5983e2350ede94e41863ee", '[]');
 INSERT INTO User (student_id, first_name, last_name, password, courses) VALUES 
-    (113539682, "SUE", "Park", "31c41d99fb6d906bf76d0c2a7406d00125d1aeff67bfa8f276d8e05dda0cac5", "");
+    (113539682, "SUE", "Park", "31c41d99fb6d906bf76d0c2a7406d00125d1aeff67bfa8f276d8e05dda0cac5", '[]');
 INSERT INTO User (student_id, first_name, last_name, password, courses) VALUES 
-    (123456789, 'John', 'Doe', "596cd873e1ebf0c7undefined9317c022b0908426f729842b395012269114bc51cfc509", "");
+    (123456789, 'John', 'Doe', "596cd873e1ebf0c7undefined9317c022b0908426f729842b395012269114bc51cfc509", '[]');
 INSERT INTO User (student_id, first_name, last_name, password, courses) VALUES 
-    (234567891, 'Jane', 'Doe', "93f28e576bb68bba43aea3fff519fec3ac029ca4c27fc3984488bee030597960", "");
+    (234567891, 'Jane', 'Doe', "93f28e576bb68bba43aea3fff519fec3ac029ca4c27fc3984488bee030597960", '[]');
 INSERT INTO User (student_id, first_name, last_name, password, courses) VALUES 
-    (345678912, 'Bob', 'Smith', "3eb1e8ede6397f63f8f8df299678be1b3ee3bd1dd58e7fb3a0c338a96da8e6a6", "");
+    (345678912, 'Bob', 'Smith', "3eb1e8ede6397f63f8f8df299678be1b3ee3bd1dd58e7fb3a0c338a96da8e6a6", '[]');
 INSERT INTO User (student_id, first_name, last_name, password, courses) VALUES 
-    (456789123, 'Alice', 'Johnson', "f2865342ddfac10e283d5a7ce609877e779ea43b260536f44d456fdcfc1c0", "");
+    (456789123, 'Alice', 'Johnson', "f2865342ddfac10e283d5a7ce609877e779ea43b260536f44d456fdcfc1c0", '[]');
 INSERT INTO User (student_id, first_name, last_name, password, courses) VALUES 
-    (567891234, 'Charlie', 'Brown', "bffea713undefined1fadcaefb581851aa02364d34b8586undefined3bb39b215f4229undefined3bba52", "");
+    (567891234, 'Charlie', 'Brown', "bffea713undefined1fadcaefb581851aa02364d34b8586undefined3bb39b215f4229undefined3bba52", '[]');
 -- INSERT INTO User (student_id, first_name, last_name, password, courses) VALUES 
 --     (678912345, 'Lucy', 'Van Pelt', "", "");
 -- INSERT INTO User (student_id, first_name, last_name, password, courses) VALUES 
@@ -79,19 +79,19 @@ INSERT INTO Courses (course_id, course_name)
 
 -- Prerequsite Table --
 INSERT INTO Prerequisites (course_id, prerequisite)
-       VALUES ("CSE101", "");
+       VALUES ("CSE101", '[]');
 INSERT INTO Prerequisites (course_id, prerequisite)
        VALUES ("CSE114", '["CSE101"]');
 INSERT INTO Prerequisites (course_id, prerequisite)
        VALUES ("CSE214", '["CSE114"]');
 INSERT INTO Prerequisites (course_id, prerequisite)
-       VALUES ("CSE215", "");
+       VALUES ("CSE215", '[]');
 INSERT INTO Prerequisites (course_id, prerequisite)
        VALUES ("CSE216", '["CSE214"]');
 INSERT INTO Prerequisites (course_id, prerequisite)
        VALUES ("CSE220", '["CSE214"]');
 INSERT INTO Prerequisites (course_id, prerequisite)
-       VALUES ("CSE300", "");
+       VALUES ("CSE300", '[]');
 INSERT INTO Prerequisites (course_id, prerequisite)
        VALUES ("CSE303", '["CSE214"]');
 INSERT INTO Prerequisites (course_id, prerequisite)
@@ -105,7 +105,7 @@ INSERT INTO Prerequisites (course_id, prerequisite)
 INSERT INTO Prerequisites (course_id, prerequisite)
        VALUES ("CSE352", '["CSE216"]');
 INSERT INTO Prerequisites (course_id, prerequisite)
-       VALUES ("CSE354", "");
+       VALUES ("CSE354", '[]');
 INSERT INTO Prerequisites (course_id, prerequisite)
        VALUES ("CSE373", '["CSE214"]');
 INSERT INTO Prerequisites (course_id, prerequisite)
