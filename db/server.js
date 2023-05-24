c7d848edfd339fdf1b1df24ebd8b305fa9268554
JuyeeMyeong <juyee.ming@gmail.com>
2023-05-24 15:57:44 +0900

    #1 get data from prerequisite

--- db/server.js
+++ db/server.js
@@ -60,6 +60,7 @@ app.get("/prerequisite", function (req, res) {
       res.status(200).json({
         status: "success",
         length: data?.length,
+        data: data[0]
       });
     }
   );
