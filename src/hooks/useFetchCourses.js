2169672cc8f6201b3c5bd00c1c96edd28dc547ed
JuyeeMyeong <juyee.ming@gmail.com>
2023-05-24 13:59:28 +0900

    #2 comment in useFetchCourses

--- src/hooks/useFetchCourses.js
+++ src/hooks/useFetchCourses.js
@@ -2,20 +2,23 @@ import { useState, useEffect } from "react";
 import axios from 'axios';
 
 export default function useFetchCourses() {
-  const [courseList, setCourseList] = useState([]);
+
+/***          GET course list from Course table & make courseList array with given course_id      ****/
+
+  const [courseList, setCourseList] = useState([]); //useState courseList
 
   useEffect(() => {
-    axios.get("http://localhost:4000/course")
+    axios.get("http://localhost:4000/course") //app.get ("/course")
       .then((response) => {
         if (response.data.status === "success") {
           const courseIds = response.data.data.map((course) => course.course_id);
-          setCourseList(courseIds);
+          setCourseList(courseIds); //setCourseList based on course_ids
         }
       })
-      .catch((error) => {
+      .catch((error) => { //failed to fetch courses
         console.error('Failed to fetch courses:', error);
       });
-  }, []);
+  }, []);s
 
   return courseList;
 }
