8fc2d302da24d103a30820e56ddc28f4b06eb700
JuyeeMyeong <juyee.ming@gmail.com>
2023-05-24 20:29:51 +0900

    #2 formatting useSearch

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default function useSearch(
   isLoggedIn,
@@ -16,41 +16,41 @@ export default function useSearch(
     if (!isLoggedIn) {
       alert("Please Login first...");
       return;
-    };
+    }
 
     try {
-        const response = await axios.post("http://localhost:4000/userCheck", {
-          stuId: stuId,
-          name: preName,
-        });
-  
-        const data = response.data;
-  
-        if (data.status === "Success") {
-            //set filtered courses & show courses (true) & setName (pass it to Search)
-            const filteredCourses = courseList.filter((course) =>
-            course.toLowerCase().includes(searchString.toLowerCase())
-            );
-            setFilteredCourses(filteredCourses);
-            setShowCourses(!showCourses);
-            setName(preName);
-            return true; // user Check!
-        } else {
-          // The server responded, but the login check failed.
-          alert("Login check failed. Please enter correct name and ID.");
-          return false;
-        }
-      } catch (error) {
-        // An error occurred during execution of the `try`.
-        if (error.response) {
-            // The server responded with a status code that falls out of the range of 2xx
-            alert("Login check failed. Please enter correct name and ID.");
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            alert("A network error occurred. Please try again later.");
-          }
-        return false; 
+      const response = await axios.post("http://localhost:4000/userCheck", {
+        stuId: stuId,
+        name: preName,
+      });
+
+      const data = response.data;
+
+      if (data.status === "Success") {
+        //set filtered courses & show courses (true) & setName (pass it to Search)
+        const filteredCourses = courseList.filter((course) =>
+          course.toLowerCase().includes(searchString.toLowerCase())
+        );
+        setFilteredCourses(filteredCourses);
+        setShowCourses(!showCourses);
+        setName(preName);
+        return true; // user Check!
+      } else {
+        // The server responded, but the login check failed.
+        alert("Login check failed. Please enter correct name and ID.");
+        return false;
       }
+    } catch (error) {
+      // An error occurred during execution of the `try`.
+      if (error.response) {
+        // The server responded with a status code that falls out of the range of 2xx
+        alert("Login check failed. Please enter correct name and ID.");
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        alert("A network error occurred. Please try again later.");
+      }
+      return false;
+    }
   };
 
   return handleSearch;
